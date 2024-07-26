e768075149b8cf848f840614f85fc02b86d96f8d
TonosDeAzul <dg.wrq109@gmail.com>
2024-07-26 13:29:08 -0500

    CRUD terminado

--- scripts/actualizar/formActualizar.js
+++ scripts/actualizar/formActualizar.js
@@ -1,4 +1,11 @@
 import traerUsuario from "../peticiones/traerUsuario.js";
+import editarUsuario from "../peticiones/editarUsuario.js";
+import evitarLetras from "../validacionInputs/evitarLetras.js";
+import evitarNumeros from "../validacionInputs/evitarNumeros.js";
+import longitudMaxima from "../validacionInputs/longitudMaxima.js";
+import longitudMinima from "../validacionInputs/longitudMinima.js";
+import validarCorreo from "../validacionInputs/validarCorreo.js";
+import tipoDocumento from "../validacionInputs/tipoDocumento.js";
 
 function crearInput(form, inputName, valorInput) {
   const _d = document;
@@ -49,7 +56,6 @@ function crearSelect(form) {
 export default function modalEditar(idUsuario) {
   const _d = document;
   const _body = _d.body;
-
   const _section = _d.createElement("section");
   _section.classList.add(
     "flex",
@@ -61,7 +67,6 @@ export default function modalEditar(idUsuario) {
     "items-center"
   );
   _body.appendChild(_section);
-
   const _form = _d.createElement("form");
   _form.classList.add(
     "text-white",
@@ -76,19 +81,72 @@ export default function modalEditar(idUsuario) {
   );
   _section.appendChild(_form);
 
-  traerUsuario(idUsuario);
-
-  crearInput(_form, "nombre");
-  crearInput(_form, "apellidos");
-  crearInput(_form, "teléfono");
-  crearInput(_form, "dirección");
-  crearSelect(_form);
-  crearInput(_form, "documento");
-  crearInput(_form, "correo");
-
-  const _button = _d.createElement("button");
-  _button.type = "submit";
-  _button.textContent = "Actualizar";
-  _button.classList.add("btn", "w-full", "bg-neutral-900", "text-white");
-  _form.appendChild(_button);
+  traerUsuario(idUsuario).then(usuario => {
+    // console.log(data);
+    crearInput(_form, "nombre", usuario.nombre);
+    crearInput(_form, "apellidos", usuario.apellidos);
+    crearInput(_form, "telefono", usuario.telefono);
+    crearInput(_form, "direccion", usuario.direccion);
+    crearSelect(_form);
+    _d.getElementById("tipoDocumento").value = usuario.tipoDocumento;
+    crearInput(_form, "documento", usuario.documento);
+    crearInput(_form, "correo", usuario.correo);
+    const _nombre = _d.getElementById("nombre");
+    const _apellidos = _d.getElementById("apellidos");
+    const _telefono = _d.getElementById("telefono");
+    const _documento = _d.getElementById("documento");
+    const _direccion = _d.getElementById("direccion");
+    const _correo = _d.getElementById("correo");
+    const _tDocumento = _d.getElementById("tipoDocumento");
+    // Evitar carácteres
+    evitarNumeros(_nombre);
+    evitarNumeros(_apellidos);
+    evitarLetras(_telefono);
+    evitarLetras(_documento);
+    // Longitud máxima
+    longitudMaxima(_nombre, 100);
+    longitudMaxima(_apellidos, 100);
+    longitudMaxima(_telefono, 10);
+    longitudMaxima(_direccion, 100);
+    longitudMaxima(_documento, 10);
+    longitudMaxima(_correo, 100);
+    const _button = _d.createElement("button");
+    _button.type = "submit";
+    _button.textContent = "Actualizar";
+    _button.classList.add("btn", "w-full", "bg-neutral-900", "text-white");
+    _form.appendChild(_button);
+    _button.addEventListener("click", (event) => {
+      event.preventDefault();
+      // Longgitud mínima
+      const longitudMinimaNombre = longitudMinima(_nombre, 3);
+      const longitudMinimaApellidos = longitudMinima(_apellidos, 3);
+      const longitudMinimaTelefono = longitudMinima(_telefono, 10);
+      const longitudMinimaDireccion = longitudMinima(_direccion, 5);
+      const longitudMinimaDocumento = longitudMinima(_documento, 8);
+      const longitudMinimaCorreo = longitudMinima(_correo, 5);
+      // Validación inputs
+      const validacionCorreo = validarCorreo(_correo);
+      const validacionTipoDocumento = tipoDocumento(_tDocumento);
+      // Validar que todos los campos cumplan con los requisitos
+      if
+        (
+        !longitudMinimaNombre || !longitudMinimaApellidos ||
+        !longitudMinimaTelefono || !longitudMinimaDireccion ||
+        !longitudMinimaDocumento || !longitudMinimaCorreo ||
+        !validacionCorreo || !validacionTipoDocumento
+      ) { return; };
+      const newData = {
+        nombre: _nombre.value,
+        apellidos: _apellidos.value,
+        telefono: _telefono.value,
+        direccion: _direccion.value,
+        tipoDocumento: _tDocumento.value,
+        documento: _documento.value,
+        correo: _correo.value,
+      };
+      editarUsuario(usuario.id, newData)
+      // Borrar datos cuando el usuario sea creado
+      _form.reset();
+    })
+  });
 };
--- scripts/peticiones/traerUsuario.js
+++ scripts/peticiones/traerUsuario.js
@@ -1,5 +1,7 @@
 export default function traerUsuario(idUsuario) {
   return fetch(`http://localhost:3000/tb_usuario/${idUsuario}`)
   .then((response) => response.json())
-  .then((data) => console.log(data));
+  .then((data) => {
+    return data
+  });
 }
